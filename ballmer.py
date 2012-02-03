fbec7d74d3e72bd163e0b0e0cffba68fafa5d429
Adam Detrick <adamd@meetup.com>
2012-02-03 18:32:31 -0500

    stub for serial monitoring

---
+++ ballmer.py
@@ -0,0 +1,11 @@
+import serial
+
+try:
+    print "Get ready to blow..."
+    arduino = serial.Serial('/dev/ttyUSB0', 9600);
+except:
+    print "Failed to connect"
+    exit()
+
+while True:
+    print arduino.readline();
