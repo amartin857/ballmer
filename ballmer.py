08a79e34a98fafaf286756daca02742231c48047
Adam Detrick <adamd@meetup.com>
2012-02-06 16:18:47 -0500

    stitching BAC message and command in subprocess

--- ballmer.py
+++ ballmer.py
@@ -1,15 +1,15 @@
 import subprocess
 import serial
 
+ARG_MSG = "message"
 SENSOR_FLAG = "foo"
 SENSOR_READING = "bar"
-COMMIT_ARGS  = "-a -m"
-SOBER_CMD = "git commit"
+SOBER_CMD = "git commit -a -m"
 TOO_DRUNK_CMD = "git stash save"
+
 
 try:
     print "Get ready to blow..."
-    subprocess.call("git status");
     arduino = serial.Serial('/dev/ttyUSB0', 9600);
 except:
     print "Failed to connect"
@@ -21,3 +21,8 @@ while True:
 
     if line.find(SENSOR_FLAG) > -1:
         print "found reading for BAC";
+        BAC = line.substr(SENSOR_READING);
+
+        commit = SOBER_CMD + " " + ARG_MSG + BAC;
+
+        #subprocess.call(commit, shell=True);
