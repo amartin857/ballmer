6058d3b9e08be8779ce2c4cfdb770f33abbf4cc1
Adam Detrick <adamd@meetup.com>
2012-02-06 15:48:25 -0500

    more setup but still need to get subprocess to work

--- ballmer.py
+++ ballmer.py
@@ -1,8 +1,15 @@
-import os
+import subprocess
 import serial
+
+SENSOR_FLAG = "foo"
+SENSOR_READING = "bar"
+COMMIT_ARGS  = "-a -m"
+SOBER_CMD = "git commit"
+TOO_DRUNK_CMD = "git stash save"
 
 try:
     print "Get ready to blow..."
+    subprocess.call("git status");
     arduino = serial.Serial('/dev/ttyUSB0', 9600);
 except:
     print "Failed to connect"
@@ -12,5 +19,5 @@ while True:
     line = arduino.readline();
     print line;
 
-    if line.find("foo") > -1:
-        print "o_0 you are drunk 0_o";
+    if line.find(SENSOR_FLAG) > -1:
+        print "found reading for BAC";
